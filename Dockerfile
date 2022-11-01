1c45b4aabc60c4cdb145fcbc76e66adbbef0333a
nialljb <nialljbourke@gmail.com>
2022-11-01 10:51:48 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -3,26 +3,23 @@ FROM flywheel/fw-gear-ants-base:2.3.5
 ENV HOME=/root/
 
 ENV FLYWHEEL="/flywheel/v0"
+RUN mkdir -p $FLYWHEEL/input
 WORKDIR $FLYWHEEL
 
 # Dependencies - some possibly dev dependencies - to run this.
 # Install git to run pre-commit hooks inside container:
 RUN apt-get update && \
-    apt-get install --no-install-recommends -y git=1:2.17.1-1ubuntu0.12 && \
+    apt-get install --no-install-recommends -y software-properties-common=0.96.20.2-2 && \
     apt-get clean && \
     pip3 install --no-cache-dir "poetry==1.1.11" && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Installing main dependencies
 COPY requirements.txt $FLYWHEEL/
-RUN pip install --no-cache-dir -r $FLYWHEEL/requirements.txt
-
-COPY fw_gear_ants_ciso $FLYWHEEL/fw_gear_ants_ciso
-COPY data ${FLYWHEEL}/input
+RUN pip3 install --no-cache-dir -r $FLYWHEEL/requirements.txt
 
 # Installing the current project (most likely to change, above layer can be cached)
 COPY ./ $FLYWHEEL/
-RUN pip install --no-cache-dir .
 
 # Configure entrypoint
 RUN chmod a+x $FLYWHEEL/run.py
---
+++ old_run.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+"""The run script."""
+import json
+import logging
+import os
+import shutil
+import sys
+from pathlib import Path
+from typing import List, Tuple, Union
+
+from flywheel_gear_toolkit import GearToolkitContext
+from flywheel_gear_toolkit.utils.file import sanitize_filename
+from flywheel_gear_toolkit.utils.metadata import Metadata
+from flywheel_gear_toolkit.utils.zip_tools import zip_output
+
+# import flywheel functions
+from fw_gear_ants_ciso.main import prepare, run
+from fw_gear_ants_ciso.parser import parse_config
+from fw_gear_ants_ciso.generate_command import generate_command
+from utils.dry_run import pretend_it_ran
+
+# The gear is split up into 2 main components. The run.py file which is executed
+# when the container runs. The run.py file then imports the rest of the gear as a
+# module.
+
+log = logging.getLogger(__name__)
+
+
+# pylint: disable=too-many-arguments
+def post_run(
+    gear_name: str,
+    gear_options: dict,
+    analysis_output_dir: Union[str, Path],
+    run_label: str,
+    errors: List[str],
+    warnings: List[str],
+) -> None:
+    """Move all the results to the final destination, clean-up.
+
+    Args:
+        gear_name (str): gear name, used in the output file names
+        gear_options (dict): gear options
+        analysis_output_dir (str or Path): name of the output dir
+        run_label (str): run label (project|subject|session label)
+        errors (list[str]): list of errors found
+        warnings (list[str]): list of warnings found
+    """
+    # zip entire output/<analysis_id> folder into
+    #  <gear_name>_<project|subject|session label>_<analysis.id>.zip
+    zip_file_name = f"{gear_name}_{run_label}_{gear_options['destination-id']}.zip"
+    zip_output(
+        str(gear_options["output-dir"]),
+        gear_options["destination-id"],
+        zip_file_name,
+        dry_run=False,
+        exclude_files=None,
+    )
+
+    # zip any .html files in output/<analysis_id>/
+    html_dir = Path(analysis_output_dir) / "cisoPrep"
+    zip_htmls(str(analysis_output_dir), gear_options["destination-id"], html_dir)
+
+    # possibly save ALL intermediate output
+    if gear_options["save-intermediate-output"]:
+        zip_all_intermediate_output(
+            gear_options["destination-id"],
+            gear_name,
+            gear_options["output-dir"],
+            gear_options["work-dir"],
+            run_label,
+        )
+
+    # possibly save intermediate files and folders
+    zip_intermediate_selected(
+        gear_options["intermediate-files"],
+        gear_options["intermediate-folders"],
+        gear_options["destination-id"],
+        gear_name,
+        gear_options["output-dir"],
+        gear_options["work-dir"],
+        run_label,
+    )
+
+    # clean up: remove output that was zipped
+    if Path(analysis_output_dir).exists():
+        if not gear_options["keep-output"]:
+
+            log.debug('removing output directory "%s"', str(analysis_output_dir))
+            shutil.rmtree(analysis_output_dir)
+
+        else:
+            log.info('NOT removing output directory "%s"', str(analysis_output_dir))
+
+    else:
+        log.info("Output directory does not exist so it cannot be removed")
+
+    # Report errors and warnings at the end of the log, so they can be easily seen.
+    if len(warnings) > 0:
+        msg = "Previous warnings:\n"
+        for warn in warnings:
+            msg += "  Warning: " + str(warn) + "\n"
+        log.info(msg)
+
+    if len(errors) > 0:
+        msg = "Previous errors:\n"
+        for err in errors:
+            if str(type(err)).split("'")[1] == "str":
+                # show string
+                msg += "  Error msg: " + str(err) + "\n"
+            else:  # show type (of error) and error message
+                err_type = str(type(err)).split("'")[1]
+                msg += f"  {err_type}: {str(err)}\n"
+        log.info(msg)
+
+def main(context: GearToolkitContext) -> None:
+    """Parses config and runs."""
+    # For now, don't allow runs at the project level:
+    destination = context.client.get(context.destination["id"])
+    if destination.parent.type == "project":
+        log.exception(
+            "This version of the gear does not run at the project level. "
+            "Try running it for each individual subject."
+        )
+        sys.exit(1)
+
+    # Errors and warnings will always be logged when they are detected.
+    # Keep a list of errors and warning to print all in one place at end of log
+    # Any errors will prevent the BIDS App from running.
+    errors = []
+    warnings = []
+
+    # Call the fw_gear_ants_ciso.parser.parse_config function
+    # to extract the args, kwargs from the context (e.g. config.json).
+    gear_inputs, gear_options, app_options = parse_config(context)
+
+    prepare_errors, prepare_warnings = prepare(
+        inputs=gear_inputs,
+        gear_options=gear_options,
+        app_options=app_options,
+    )
+    errors += prepare_errors
+    warnings += prepare_warnings
+
+    if len(errors) == 0:
+        tree_title = f"{sanitize_filename(gear_options['kcl-app-binary'])} BIDS Tree"
+        subject_label, run_label, get_bids_errors = get_bids_data(
+            context=context,
+            gear_options=gear_options,
+            tree_title=tree_title,
+        )
+        errors += get_bids_errors
+
+        # For BIDS-Apps that run at the participant level, set the
+        # "participant_label" from the container from which it was launched.
+        if gear_options["analysis-level"] == "participant" and not app_options.get(
+            "participant_label", ""
+        ):
+            app_options["participant_label"] = subject_label
+
+        # In general, BIDS-Apps take only the (subject) label, without the "sub-" part:
+        if app_options["participant_label"].startswith("sub-"):
+            # Write this in two instructions because if you write it in one, Black will
+            # format it horribly:
+            new_participant_label = app_options["participant_label"][len("sub-") :]
+            app_options["participant_label"] = new_participant_label
+
+    else:
+        run_label = "error"
+        log.info("Did not download BIDS because of previous errors")
+        print(errors)
+
+    if len(errors) > 0:
+        e_code = 1
+        log.info("Command was NOT run because of previous errors.")
+
+    elif gear_options["dry-run"]:
+        e_code = 0
+        pretend_it_ran(gear_options, app_options)
+        save_metadata(
+            context,
+            gear_options["output_analysis_id_dir"] / "qsiprep",
+            {"dry-run": "true"},
+        )
+        e = "gear-dry-run is set: Command was NOT run."
+        log.warning(e)
+        warnings.append(e)
+
+    else:
+        try:
+            # Pass the args, kwargs to fw_gear_qsiprep.main.run function to execute
+            # the main functionality of the gear.
+            e_code = run(gear_options, app_options)
+
+        except RuntimeError as exc:
+            e_code = 1
+            errors.append(str(exc))
+            log.critical(exc)
+            log.exception("Unable to execute command.")
+
+        else:
+            # We want to save the metadata only if the run was successful.
+            # We want to save partial outputs in the event of the app crashing, because
+            # the partial outputs can help pinpoint what the exact problem was. So we
+            # have `post_run` further down.
+            save_metadata(context, gear_options["output_analysis_id_dir"] / "cisoPrep")
+
+    # Cleanup, move all results to the output directory.
+    # post_run should be run regardless of dry-run or exit code.
+    # It will be run even in the event of an error, so that the partial results are
+    # available for debugging.
+    post_run(
+        gear_name=context.manifest["name"],
+        gear_options=gear_options,
+        analysis_output_dir=str(gear_options["output_analysis_id_dir"]),
+        run_label=run_label,
+        errors=errors,
+        warnings=warnings,
+    )
+
+    gear_builder = context.manifest.get("custom").get("gear-builder")
+    # gear_builder.get("image") should be something like:
+    # flywheel/bids-qsiprep:0.0.1_0.15.1
+    container = gear_builder.get("image").split(":")[0]
+    log.info("%s Gear is done.  Returning %s", container, e_code)
+
+    # Exit the python script (and thus the container) with the exit
+    # code returned by fw_gear_bids_qsiprep.main.run function.
+    sys.exit(e_code)
+
+
+# pylint: enable=too-many-locals,too-many-statements
+
+
+# Only execute if file is run as main, not when imported by another module
+if __name__ == "__main__":  # pragma: no cover
+    # Get access to gear config, inputs, and sdk client if enabled.
+    with GearToolkitContext() as gear_context:
+
+        # Initialize logging, set logging level based on `debug` configuration
+        # key in gear config.
+        gear_context.init_logging()
+
+        # Pass the gear context into main function defined above.
+        main(gear_context)
